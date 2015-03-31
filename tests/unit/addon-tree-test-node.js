94e55dd4debc084450b616794dc44cd2fc6029ff
ramyb <rami@dudamobile.com>
2015-03-31 03:28:27 +0300

    missing semicolon

--- tests/unit/addon-tree-test-node.js
+++ tests/unit/addon-tree-test-node.js
@@ -52,7 +52,7 @@ describe('Addon', function() {
 
     it('returns an empty tree when disable option is true', function() {
       process.env.EMBER_ENV = 'production';
-      var addon = getMirageAddon({ configPath: 'tests/fixtures/config/environment-with-disable-false' })
+      var addon = getMirageAddon({ configPath: 'tests/fixtures/config/environment-with-disable-false' });
       var addonTree = addon.treeFor('addon');
 
       expect(addonTree.inputTrees.length).to.be.equal(0);

1acf2f11f861a91a9370966714eefc1ccb16128d
James Dixon <jim.w.dixon@gmail.com>
2015-08-10 02:51:12 -0600

    adds support for urlPrefix to allow requests to external resources; fixes #228

--- tests/integration/server-config-test.js
+++ tests/integration/server-config-test.js
@@ -34,3 +34,46 @@ test("namespace can be configured", function(assert) {
     done();
   });
 });
+
+test("urlPrefix can be configured", function(assert) {
+  assert.expect(1);
+  var done = assert.async();
+  var server = this.server;
+
+  var contacts = [
+    {id: 1, name: 'Link'},
+    {id: 2, name: 'Zelda'},
+  ];
+  server.db.loadData({
+    contacts: contacts
+  });
+  server.urlPrefix = 'http://localhost:3000';
+  server.get('/contacts');
+
+  $.getJSON('http://localhost:3000/contacts', function(data) {
+    assert.deepEqual(data, { contacts: contacts });
+    done();
+  });
+});
+
+test("urlPrefix and namespace can be configured simultaneously", function(assert) {
+  assert.expect(1);
+  var done = assert.async();
+  var server = this.server;
+
+  var contacts = [
+    {id: 1, name: 'Link'},
+    {id: 2, name: 'Zelda'},
+  ];
+  server.db.loadData({
+    contacts: contacts
+  });
+  server.urlPrefix = 'http://localhost:3000';
+  server.namespace = 'api';
+  server.get('/contacts');
+
+  $.getJSON('http://localhost:3000/api/contacts', function(data) {
+    assert.deepEqual(data, { contacts: contacts });
+    done();
+  });
+});

aaaa0927fd7905d7ce299e3a89a655ad340bf748
Jon Greene <jon.greene@corvisa.com>
2016-05-09 17:00:51 -0500

    preventing generators from writing new files to the add-on's test/dummy directory.

--- blueprints/mirage-factory/index.js
+++ blueprints/mirage-factory/index.js
@@ -9,9 +9,9 @@ module.exports = {
   fileMapTokens: function() {
     return {
       __root__: function(options) {
-        if (options.inAddon) {
-          return path.join('tests', 'dummy');
-        }
+        // if (options.inAddon) {
+        //   return path.join('tests', 'dummy');
+        // }
 
         return '/';
       }

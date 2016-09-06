a8143a0f9dfb131d60f8d53177b90e1fb460554e
Leooo <viet_lionel@yahoo.fr>
2016-09-06 15:10:54 +0100

    small fix

--- tests/dummy/app/adapters/application.js
+++ tests/dummy/app/adapters/application.js
@@ -1,8 +1,4 @@
 import ActiveModelAdapter from 'active-model-adapter';
 import Ember from 'ember';
 
-export default ActiveModelAdapter.extend({
-  pathForType(type) {
-    return Ember.String.pluralize(Ember.String.dasherize(type));
-  }
-});
+export default ActiveModelAdapter;

2439838268a4fc0aa9130622c4421e3109dc4f22
Dan Freeman <dfreeman@salsify.com>
2017-10-09 14:08:41 -0400

    Support `Class.extend()` in ES6 environments

--- addon/utils/extend.js
+++ addon/utils/extend.js
@@ -3,38 +3,20 @@ import _has from 'lodash/has';
 
 export default function(protoProps, staticProps) {
   let parent = this;
-  let child;
-
-  // The constructor function for the new subclass is either defined by you
-  // (the "constructor" property in your `extend` definition), or defaulted
-  // by us to simply call the parent's constructor.
-  if (protoProps && _has(protoProps, 'constructor')) {
-    child = protoProps.constructor;
-  } else {
-    child = function() {
-      return parent.apply(this, arguments);
-    };
-  }
-
-  // Add static properties to the constructor function, if supplied.
-
-  _assign(child, parent, staticProps);
-
-  // Set the prototype chain to inherit from `parent`, without calling
-  // `parent`'s constructor function.
-  let Surrogate = function() {
-    this.constructor = child;
+  let child = class extends parent {
+    // The constructor function for the new subclass is either defined by you
+    // (the "constructor" property in your `extend` definition), or defaulted
+    // by us to simply call the parent's constructor.
+    constructor(...params) {
+      super(...params);
+      if (protoProps && _has(protoProps, 'constructor')) {
+        return protoProps.constructor.apply(this, params);
+      }
+    }
   };
 
-  Surrogate.prototype = parent.prototype;
-  child.prototype = new Surrogate();
-
-  // Add prototype properties (instance properties) to the subclass,
-  // if supplied.
-  if (protoProps) {
-    _assign(child.prototype, protoProps);
-  }
-  // if (protoProps) { _assign(child.prototype, protoProps); }
+  _assign(child, staticProps);
+  _assign(child.prototype, protoProps);
 
   // Set a convenience property in case the parent's prototype is needed
   // later.

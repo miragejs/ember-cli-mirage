62086eb6387cd3cba593de2efa9902e556f1404a
Sam Selikoff <sam.selikoff@gmail.com>
2018-07-01 21:53:57 -0400

    Need to figure out inflector

--- addon/utils/inflector.js
+++ addon/utils/inflector.js
@@ -3,6 +3,17 @@ import dasherize from 'lodash/kebabCase';
 import underscore from 'lodash/snakeCase';
 import camelCase from 'lodash/camelCase';
 
+// import Inflector from 'ember-inflector';
+// import defaultRules from 'ember-inflector/addon/lib/system/inflections';
+// // debugger;
+// const inflector = new Inflector(defaultRules);
+// const singularize = inflector.singularize.bind(inflector);
+// const pluralize = inflector.pluralize.bind(inflector);
+
+import { singularize, pluralize } from 'ember-inflector';
+// import { singularize, pluralize } from 'ember-inflector/addon/index';
+// import singularize from 'ember-inflector/addon/index'
+
 /*
   Lodash's camelCase removes slashes, Ember's doesn't. We originally used
   Ember.String.camelize.
@@ -13,5 +24,5 @@ const camelize = str => {
     .join('/');
 };
 
-export { singularize, pluralize } from 'ember-inflector';
+export { singularize, pluralize };
 export { capitalize, camelize, dasherize, underscore };

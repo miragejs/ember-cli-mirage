67387d45265a71c3738212e2738caa36ccd7c68c
Sam Selikoff <sam.selikoff@gmail.com>
2019-01-29 11:31:06 -0500

    Feat: Add auto-import, remove pretender shim
    Closes #1469

---
+++ test-projects/02-app-that-excludes-mirage/tests/acceptance/modules-test.js
@@ -0,0 +1,14 @@
+import { module, test } from 'qunit';
+import { visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | modules', function(hooks) {
+  setupApplicationTest(hooks);
+
+  test('only 1 module (the no-op initializer) is included in the build', async function(assert) {
+    await visit('/');
+
+    assert.dom('[data-test-id="mirage-module-count"]').hasText('0');
+    assert.dom('[data-test-id="other-module-count"]').hasText('1');
+  });
+});

2ddb4aa4a458ea20b8ec5691ea4f2790b1172e74
Alexandre Santos <alexandre.santozz@gmail.com>
2019-05-27 15:18:41 +0100

    Forward ember-inflector calls to inflected

--- addon/index.js
+++ addon/index.js
@@ -1,4 +1,26 @@
 import MirageServer from '@miragejs/server/lib';
 export * from '@miragejs/server/lib';
 
+import Inflector from 'ember-inflector';
+import { __inflections } from './utils/inflector';
+
+function patchEmberInflector() {
+  __inflections(inflector => {
+    // Proxy ember-inflector calls to `inflected`
+    Inflector.inflector = new window.Proxy(Inflector.inflector, {
+      get: function(_, prop) {
+        if (inflector[prop]) {
+          return function(...args) {
+            return inflector[prop](...args);
+          };
+        }
+
+        return window.Reflect.get(...arguments);
+      }
+    });
+  });
+}
+
+patchEmberInflector();
+
 export default MirageServer;

a353fb569256ff3d5eff67ede7e52e227515d89e
Alexandre Santos <alexandre.santozz@gmail.com>
2019-05-28 16:55:12 +0100

    Revert changes in collection-inflector-test

--- tests/integration/orm/collection-inflector-test.js
+++ tests/integration/orm/collection-inflector-test.js
@@ -1,14 +1,14 @@
 import Model from 'ember-cli-mirage/orm/model';
 import Schema from 'ember-cli-mirage/orm/schema';
 import Db from 'ember-cli-mirage/db';
+import Inflector from 'ember-inflector';
 import {module, test} from 'qunit';
-import { __inflections } from 'ember-cli-mirage/utils/inflector';
 
 var db, schema, HeadOfState;
 
 module('Integration | ORM | inflector-collectionName integration', function(hooks) {
   hooks.beforeEach(function() {
-    __inflections(inflector => inflector.irregular('head-of-state', 'heads-of-state'));
+    Inflector.inflector.irregular('head-of-state', 'heads-of-state');
 
     HeadOfState = Model.extend();
     db = new Db({});

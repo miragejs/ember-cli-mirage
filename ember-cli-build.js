1ca951bb5d30f5de9c89ba3592060190d2cdb3a6
Alexandre Santos <alexandre.santozz@gmail.com>
2019-06-04 03:22:10 +0100

    Add packages to ember-cli-build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,11 +7,15 @@ module.exports = function(defaults) {
     // Add options here
     'ember-cli-babel': {
       includePolyfill: true
+    },
+    'ember-cli-addon-docs': {
+      packages: ['./node_modules/@miragejs/server']
     }
   });
 
   /*
     This build file specifies the options for the dummy test app of this
+
     addon, located in `/tests/dummy`
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file

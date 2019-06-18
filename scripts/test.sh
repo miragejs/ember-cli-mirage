5739683a73c3f85d5f2e114f78e86dcea69cf79f
Sam Selikoff <sam.selikoff@gmail.com>
2019-06-18 11:27:06 -0400

    Actually run test project tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,5 +2,6 @@
 
 ./scripts/parallel --tag <<EOF
 cd test-projects/01-basic-app && yarn test:fastboot
+cd test-projects/01-basic-app && yarn test
 cd test-projects/02-app-that-excludes-mirage && yarn test
 EOF

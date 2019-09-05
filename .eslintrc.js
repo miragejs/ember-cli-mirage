a29742a8d355d44c039943e0d28cae3d496ad8ae
Sam Selikoff <sam.selikoff@gmail.com>
2019-09-05 00:48:13 -0400

    Bump mirage 2 (#1741)
    * Bump mirage, remove pretender
    
    * Specify sourceDirectory for mirage
    
    * Bump addon-docs-esdoc
    
    * Remember to shutdown server in test
    
    * remove ember-suave
    
    * Update assertions

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,22 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: "module"
   },
-  plugins: [
-    'ember'
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember-suave/recommended'
-  ],
+  plugins: ["ember"],
+  extends: ["eslint:recommended"],
   env: {
     browser: true
   },
   rules: {
-    'camelcase': 0,
-    'ember-suave/no-direct-property-access': 0,
-    'ember-suave/prefer-destructuring': 0,
-    'object-curly-spacing': 0,
-    'quotes': 0,
-    'array-bracket-spacing': 0,
-    'no-var': 0,
-    'object-shorthand': 0,
-    'arrow-parens': 0,
-    'no-unused-vars': ['error', { 'args': 'none' }]
+    camelcase: 0,
+    "object-curly-spacing": 0,
+    quotes: 0,
+    "array-bracket-spacing": 0,
+    "no-var": 0,
+    "object-shorthand": 0,
+    "arrow-parens": 0,
+    "no-unused-vars": ["error", { args: "none" }]
   },
   globals: {
     server: true
@@ -33,42 +26,46 @@ module.exports = {
     // node files
     {
       files: [
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'index.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'tests/dummy/config/**/*.js',
-        'fastboot-tests/**/*.js'
+        ".template-lintrc.js",
+        "ember-cli-build.js",
+        "index.js",
+        "testem.js",
+        "blueprints/*/index.js",
+        "config/**/*.js",
+        "tests/dummy/config/**/*.js",
+        "fastboot-tests/**/*.js"
       ],
       excludedFiles: [
-        'addon/**',
-        'addon-test-support/**',
-        'app/**',
-        'tests/dummy/app/**'
+        "addon/**",
+        "addon-test-support/**",
+        "app/**",
+        "tests/dummy/app/**"
       ],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
         ecmaVersion: 2018
       },
       env: {
         browser: false,
         node: true
       },
-      plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        // add your custom rules and overrides for node files here
-      })
+      plugins: ["node"],
+      rules: Object.assign(
+        {},
+        require("eslint-plugin-node").configs.recommended.rules,
+        {
+          // add your custom rules and overrides for node files here
+        }
+      )
     },
 
     // test files. Can remove when we upgrade tests to new style.
     {
-      files: ['tests/**/*.js'],
-      excludedFiles: ['tests/dummy/**/*.js'],
+      files: ["tests/**/*.js"],
+      excludedFiles: ["tests/dummy/**/*.js"],
       env: {
         embertest: true
       }
-    },
+    }
   ]
 };
--- tests/integration/serializers/json-api-serializer/associations/links-test.js
+++ tests/integration/serializers/json-api-serializer/associations/links-test.js
@@ -1,171 +1,182 @@
-import SerializerRegistry from 'ember-cli-mirage/serializer-registry';
-import { Server, Model, hasMany, belongsTo, JSONAPISerializer, ActiveModelSerializer } from 'ember-cli-mirage';
-import { module, test } from 'qunit';
-import Db from 'ember-cli-mirage/db';
-import Schema from 'ember-cli-mirage/orm/schema';
+import SerializerRegistry from "ember-cli-mirage/serializer-registry";
+import {
+  Server,
+  Model,
+  hasMany,
+  belongsTo,
+  JSONAPISerializer,
+  ActiveModelSerializer
+} from "ember-cli-mirage";
+import { module, test } from "qunit";
+import Db from "ember-cli-mirage/db";
+import Schema from "ember-cli-mirage/orm/schema";
 
-module('Integration | Serializers | JSON API Serializer | Associations | Links', function(hooks) {
-  hooks.beforeEach(function() {
-    this.schema = new Schema(new Db(), {
-      wordSmith: Model.extend({
-        blogPosts: hasMany()
-      }),
-      blogPost: Model.extend({
-        wordSmith: belongsTo(),
-        fineComments: hasMany()
-      }),
-      fineComment: Model.extend({
-        blogPost: belongsTo()
-      })
-    });
-  });
-
-  hooks.afterEach(function() {
-    this.schema.db.emptyData();
-  });
-
-  test(`it supports links`, function(assert) {
-    let registry = new SerializerRegistry(this.schema, {
-      application: JSONAPISerializer,
-      blogPost: JSONAPISerializer.extend({
-        links(model) {
-          return {
-            'wordSmith': {
-              related: `/api/word_smiths/${model.wordSmith.id}`,
-              self: `/api/blog_posts/${model.id}/relationships/word_smith`
-            },
-            'fineComments': {
-              related: `/api/fine_comments?blog_post_id=${model.id}`,
-              self: `/api/blog_posts/${model.id}/relationships/fine_comments`
-            }
-          };
-        }
-      })
-    });
-
-    let link = this.schema.wordSmiths.create({ id: 3, name: 'Link' }); // specify id to really test our links function
-    let blogPost = link.createBlogPost({ title: 'Lorem ipsum' });
-
-    let result = registry.serialize(blogPost);
-
-    assert.deepEqual(result, {
-      data: {
-        type: 'blog-posts',
-        id: blogPost.id,
-        attributes: {
-          'title': 'Lorem ipsum'
-        },
-        relationships: {
-          'word-smith': {
-            links: {
-              related: `/api/word_smiths/${link.id}`,
-              self: `/api/blog_posts/${blogPost.id}/relationships/word_smith`
-            }
-          },
-          'fine-comments': {
-            links: {
-              related: `/api/fine_comments?blog_post_id=${blogPost.id}`,
-              self: `/api/blog_posts/${blogPost.id}/relationships/fine_comments`
-            }
-          }
-        }
-      }
-    });
-  });
-
-  test(`it supports links alongside data linkage`, function(assert) {
-    let ApplicationSerializer = JSONAPISerializer.extend({
-      alwaysIncludeLinkageData: true
-    });
-    let registry = new SerializerRegistry(this.schema, {
-      application: ApplicationSerializer,
-      blogPost: ApplicationSerializer.extend({
-        links(model) {
-          return {
-            'wordSmith': {
-              related: `/api/word_smiths/${model.wordSmith.id}`,
-              self: `/api/blog_posts/${model.id}/relationships/word_smith`
-            },
-            'fineComments': {
-              related: `/api/fine_comments?blog_post_id=${model.id}`,
-              self: `/api/blog_posts/${model.id}/relationships/fine_comments`
-            }
-          };
-        }
-      })
-    });
-
-    let link = this.schema.wordSmiths.create({ id: 3, name: 'Link' }); // specify id to really test our links function
-    let blogPost = link.createBlogPost({ title: 'Lorem ipsum' });
-
-    let result = registry.serialize(blogPost);
-
-    assert.deepEqual(result, {
-      data: {
-        type: 'blog-posts',
-        id: blogPost.id,
-        attributes: {
-          'title': 'Lorem ipsum'
-        },
-        relationships: {
-          'word-smith': {
-            "data": {
-              "id": "3",
-              "type": "word-smiths"
-            },
-            links: {
-              related: `/api/word_smiths/${link.id}`,
-              self: `/api/blog_posts/${blogPost.id}/relationships/word_smith`
-            }
-          },
-          'fine-comments': {
-            data: [],
-            links: {
-              related: `/api/fine_comments?blog_post_id=${blogPost.id}`,
-              self: `/api/blog_posts/${blogPost.id}/relationships/fine_comments`
-            }
-          }
-        }
-      }
-    });
-  });
-
-  test('[regression] it works when using a named serializer, relationships, and a non-JSONAPISerializer for the base', async function(assert) {
-    let server = new Server({
-      serializers: {
-        application: ActiveModelSerializer,
-        userV2: JSONAPISerializer
-      },
-
-      models: {
-        user: Model.extend({
-          posts: hasMany()
+module(
+  "Integration | Serializers | JSON API Serializer | Associations | Links",
+  function(hooks) {
+    hooks.beforeEach(function() {
+      this.schema = new Schema(new Db(), {
+        wordSmith: Model.extend({
+          blogPosts: hasMany()
         }),
-        post: Model.extend()
-      },
-
-      baseConfig() {
-        this.get('/users', function(schema) {
-          let json = this.serialize(schema.users.find(1), 'userV2');
-
-          return json;
-        });
-      }
+        blogPost: Model.extend({
+          wordSmith: belongsTo(),
+          fineComments: hasMany()
+        }),
+        fineComment: Model.extend({
+          blogPost: belongsTo()
+        })
+      });
     });
 
-    server.create('user');
+    hooks.afterEach(function() {
+      this.schema.db.emptyData();
+    });
 
-    let res = await fetch('/users');
-    let json = await res.json();
+    test(`it supports links`, function(assert) {
+      let registry = new SerializerRegistry(this.schema, {
+        application: JSONAPISerializer,
+        blogPost: JSONAPISerializer.extend({
+          links(model) {
+            return {
+              wordSmith: {
+                related: `/api/word_smiths/${model.wordSmith.id}`,
+                self: `/api/blog_posts/${model.id}/relationships/word_smith`
+              },
+              fineComments: {
+                related: `/api/fine_comments?blog_post_id=${model.id}`,
+                self: `/api/blog_posts/${model.id}/relationships/fine_comments`
+              }
+            };
+          }
+        })
+      });
 
-    assert.deepEqual(
-      json,
-      {
-        "data": {
-          "attributes": {},
-          "id": "1",
-          "type": "users"
+      let link = this.schema.wordSmiths.create({ id: 3, name: "Link" }); // specify id to really test our links function
+      let blogPost = link.createBlogPost({ title: "Lorem ipsum" });
+
+      let result = registry.serialize(blogPost);
+
+      assert.deepEqual(result, {
+        data: {
+          type: "blog-posts",
+          id: blogPost.id,
+          attributes: {
+            title: "Lorem ipsum"
+          },
+          relationships: {
+            "word-smith": {
+              links: {
+                related: `/api/word_smiths/${link.id}`,
+                self: `/api/blog_posts/${blogPost.id}/relationships/word_smith`
+              }
+            },
+            "fine-comments": {
+              links: {
+                related: `/api/fine_comments?blog_post_id=${blogPost.id}`,
+                self: `/api/blog_posts/${blogPost.id}/relationships/fine_comments`
+              }
+            }
+          }
         }
       });
-  });
-});
+    });
+
+    test(`it supports links alongside data linkage`, function(assert) {
+      let ApplicationSerializer = JSONAPISerializer.extend({
+        alwaysIncludeLinkageData: true
+      });
+      let registry = new SerializerRegistry(this.schema, {
+        application: ApplicationSerializer,
+        blogPost: ApplicationSerializer.extend({
+          links(model) {
+            return {
+              wordSmith: {
+                related: `/api/word_smiths/${model.wordSmith.id}`,
+                self: `/api/blog_posts/${model.id}/relationships/word_smith`
+              },
+              fineComments: {
+                related: `/api/fine_comments?blog_post_id=${model.id}`,
+                self: `/api/blog_posts/${model.id}/relationships/fine_comments`
+              }
+            };
+          }
+        })
+      });
+
+      let link = this.schema.wordSmiths.create({ id: 3, name: "Link" }); // specify id to really test our links function
+      let blogPost = link.createBlogPost({ title: "Lorem ipsum" });
+
+      let result = registry.serialize(blogPost);
+
+      assert.deepEqual(result, {
+        data: {
+          type: "blog-posts",
+          id: blogPost.id,
+          attributes: {
+            title: "Lorem ipsum"
+          },
+          relationships: {
+            "word-smith": {
+              data: {
+                id: "3",
+                type: "word-smiths"
+              },
+              links: {
+                related: `/api/word_smiths/${link.id}`,
+                self: `/api/blog_posts/${blogPost.id}/relationships/word_smith`
+              }
+            },
+            "fine-comments": {
+              data: [],
+              links: {
+                related: `/api/fine_comments?blog_post_id=${blogPost.id}`,
+                self: `/api/blog_posts/${blogPost.id}/relationships/fine_comments`
+              }
+            }
+          }
+        }
+      });
+    });
+
+    test("[regression] it works when using a named serializer, relationships, and a non-JSONAPISerializer for the base", async function(assert) {
+      let server = new Server({
+        environment: "test",
+        serializers: {
+          application: ActiveModelSerializer,
+          userV2: JSONAPISerializer
+        },
+
+        models: {
+          user: Model.extend({
+            posts: hasMany()
+          }),
+          post: Model.extend()
+        },
+
+        baseConfig() {
+          this.get("/users", function(schema) {
+            let json = this.serialize(schema.users.find(1), "userV2");
+
+            return json;
+          });
+        }
+      });
+
+      server.create("user");
+
+      let res = await fetch("/users");
+      let json = await res.json();
+
+      assert.deepEqual(json, {
+        data: {
+          attributes: {},
+          id: "1",
+          type: "users"
+        }
+      });
+
+      server.shutdown();
+    });
+  }
+);

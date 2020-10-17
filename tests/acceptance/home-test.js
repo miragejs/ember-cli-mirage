aa7283340291202ee5b98300131b308e66c31310
Brian Gantzler <brian.gantzler@cardinalhealth.com>
2020-10-17 16:29:49 -0400

    Only run acceptance test for ember-source > 3.16.0 (#2067)

--- tests/acceptance/home-test.js
+++ tests/acceptance/home-test.js
@@ -1,13 +1,20 @@
 import { module, test } from "qunit";
 import { visit, currentURL } from "@ember/test-helpers";
 import { setupApplicationTest } from "ember-qunit";
+import {
+  // General functions for checking against Ember version
+  gte
+} from 'ember-compatibility-helpers';
 
 module("Acceptance | home", function(hooks) {
   setupApplicationTest(hooks);
 
-  test("the homepage renders without error", async function(assert) {
-    await visit("/");
+  //  Ember Addon docs will fail for sources less then 3.16.0
+  if (gte('3.16.0')) {
+    test("the homepage renders without error", async function(assert) {
+      await visit("/");
 
-    assert.equal(currentURL(), "/");
-  });
+      assert.equal(currentURL(), "/", "Correct URL is shown");
+    });
+  }
 });

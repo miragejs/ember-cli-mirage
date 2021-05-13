a0f44fd6cf8c31cb8fb140684db075b216aac65c
Travis Hoover <thoov7@gmail.com>
2021-05-13 14:36:06 -0700

    Add @embroider/test-setup to scenarios
    Adds two new scenarios for ember-try: embroider-safe and embroider-optimized.
    Embroider-safe does not enable any tree shaking optimizations where as
    embroider-optimized enables them all.

--- addon/get-rfc232-test-context.js
+++ addon/get-rfc232-test-context.js
@@ -1,4 +1,5 @@
-import require from 'require';
+import { macroCondition, dependencySatisfies, importSync } from '@embroider/macros';
+
 
 /**
   Helper to get our rfc232/rfc268 test context object, or null if we're not in
@@ -10,8 +11,9 @@ export default function getRfc232TestContext() {
   // Support older versions of `ember-qunit` that don't have
   // `@ember/test-helpers` (and therefore cannot possibly be running an
   // rfc232/rfc268 test).
-  if (require.has('@ember/test-helpers')) {
-    let { getContext } = require('@ember/test-helpers');
+  if (macroCondition(dependencySatisfies('@ember/test-helpers', '*'))) {
+    let { getContext } = importSync('@ember/test-helpers');
+
     return getContext();
   }
 }
--- config/ember-try.js
+++ config/ember-try.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
+const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = function() {
   return Promise.all([
@@ -61,7 +62,9 @@ module.exports = function() {
               '@ember/jquery': '^0.5.1'
             }
           }
-        }
+        },
+        embroiderSafe(),
+        embroiderOptimized(),
       ]
     };
   });

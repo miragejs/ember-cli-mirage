3a62e09bb3fb8a1404cfc5fe3381b2d01c659cce
Nathanael Dekeister <nathanael_dekeister@ultimatesoftware.com>
2021-07-09 01:42:13 +0200

    fix: ember-qunit dependencies check

--- addon-test-support/index.js
+++ addon-test-support/index.js
@@ -1,9 +1,9 @@
 import setupMirage from './setup-mirage';
 export { setupMirage };
 
-import { dependencySatisfies } from '@embroider/macros';
+import { dependencySatisfies, macroCondition } from '@embroider/macros';
 
-if (dependencySatisfies('ember-qunit', '*')) {
+if (macroCondition(dependencySatisfies('ember-qunit', '*'))) {
   window.QUnit.config.urlConfig.push({ id: 'mirageLogging', label: 'Mirage logging', });
 }
 

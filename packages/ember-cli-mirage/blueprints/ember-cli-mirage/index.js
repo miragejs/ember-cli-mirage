3977fee98eb3f8b994b06b1e6942d80e71187d48
Brian Gantzler <brian.gantzler@cardinalhealth.com>
2022-05-22 10:59:41 -0400

    Fixed lint for prettier

--- packages/ember-cli-mirage/blueprints/ember-cli-mirage/index.js
+++ packages/ember-cli-mirage/blueprints/ember-cli-mirage/index.js
@@ -30,8 +30,6 @@ module.exports = {
   },
 
   afterInstall: function () {
-      return this.addPackagesToProject([
-        { name: 'miragejs' },
-      ]);
+    return this.addPackagesToProject([{ name: 'miragejs' }]);
   },
 };

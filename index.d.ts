5c380297de53a139d6789667d268dd44be431c28
Charles Fries <charliefries@icloud.com>
2022-09-23 13:59:02 -0700

    feat: add `discoverEmberDataModels`, `applyEmberDataSerializers` TS declarations

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+import type { AnyModels } from "miragejs/-types";
+
+export function discoverEmberDataModels(): AnyModels;
+export function applyEmberDataSerializers(mirageSerializers: {}): unknown;

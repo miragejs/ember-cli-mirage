2c70fe432ca34c87e0a1b395ef74fe5ec105dc77
Sergey Astapov <SergeAstapov@gmail.com>
2023-03-31 16:05:00 -0400

    run `npx ember-cli-update --to=4.11.0` in test apps to align with blueprint

--- test-packages/01-basic-app/ember-cli-build.js
+++ test-packages/01-basic-app/ember-cli-build.js
@@ -3,8 +3,14 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  let app = new EmberApp(defaults, {
-    // Add options here
+  const app = new EmberApp(defaults, {
+    '@embroider/macros': {
+      setConfig: {
+        '@ember-data/store': {
+          polyfillUUID: true,
+        },
+      },
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated

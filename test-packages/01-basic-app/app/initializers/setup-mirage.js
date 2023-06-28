e4c6e426e8f708760921300ae6a4750bcb124a58
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-06-28 18:30:59 -0400

    Make variables used in the basic app's setup-mirage initializer match the docs

--- test-packages/01-basic-app/app/initializers/setup-mirage.js
+++ test-packages/01-basic-app/app/initializers/setup-mirage.js
@@ -1,11 +1,11 @@
 import config from 'basic-app/config/environment';
 
 import { setupMirage } from 'ember-cli-mirage';
-import mirageConfig from 'basic-app/mirage/config';
+import makeServer from 'basic-app/mirage/config';
 
 export default {
   name: 'setup-mirage',
   initialize(application) {
-    setupMirage(config, mirageConfig, application);
+    setupMirage(config, makeServer, application);
   },
 };

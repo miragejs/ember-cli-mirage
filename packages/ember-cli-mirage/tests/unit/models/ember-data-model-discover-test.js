941d1888dc02187dd3b28089894535f68a6a5dce
Sergey Astapov <sergeastapov@gmail.com>
2023-10-04 22:30:54 -0400

    fix more ember-data v5 deprecations

--- packages/ember-cli-mirage/tests/unit/models/ember-data-model-discover-test.js
+++ packages/ember-cli-mirage/tests/unit/models/ember-data-model-discover-test.js
@@ -2,7 +2,6 @@ import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import { createServer } from 'miragejs';
 import { discoverEmberDataModels } from 'ember-cli-mirage';
-import { dependencySatisfies } from '@embroider/macros';
 
 module('Unit | Model | ember data model discover', function (hooks) {
   setupTest(hooks);
@@ -20,14 +19,11 @@ module('Unit | Model | ember data model discover', function (hooks) {
     server.shutdown();
   });
 
-  (dependencySatisfies('ember-data', '>=5.0.0') ? test.todo : test)(
-    'it discovers the models',
-    function (assert) {
-      assert.ok(server.schema.modelFor('address'));
-      assert.ok(server.schema.modelFor('comment'));
-      assert.ok(server.schema.modelFor('post'));
-      assert.ok(server.schema.modelFor('user'));
-      assert.ok(server.schema.modelFor('wordSmith'));
-    },
-  );
+  test('it discovers the models', function (assert) {
+    assert.ok(server.schema.modelFor('address'));
+    assert.ok(server.schema.modelFor('comment'));
+    assert.ok(server.schema.modelFor('post'));
+    assert.ok(server.schema.modelFor('user'));
+    assert.ok(server.schema.modelFor('wordSmith'));
+  });
 });

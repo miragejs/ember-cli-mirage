ffe05a7769f741915f3e9c6f8cb170e07b1d047c
Sergey Astapov <sergeastapov@gmail.com>
2023-10-12 21:03:27 -0400

    Restore usingProxy config

---
+++ packages/ember-cli-mirage/config/environment.js
@@ -0,0 +1,31 @@
+'use strict';
+const fs = require('fs');
+
+function usingProxy() {
+  const usingProxyArg = !!process.argv.filter(function (arg) {
+    return (
+      arg.indexOf('--proxy') === 0 ||
+      arg.indexOf('-pr') === 0 ||
+      arg.indexOf('-pxy') === 0
+    );
+  }).length;
+
+  let hasGeneratedProxies = false;
+  const proxiesDir = `${process.env.PWD}/server/proxies`;
+  try {
+    fs.lstatSync(proxiesDir);
+    hasGeneratedProxies = true;
+  } catch (e) {
+    // ignore
+  }
+
+  return usingProxyArg || hasGeneratedProxies;
+}
+
+module.exports = function (environment, appConfig) {
+  appConfig['ember-cli-mirage'] = appConfig['ember-cli-mirage'] || {};
+  appConfig['ember-cli-mirage'].usingProxy = usingProxy();
+  appConfig['ember-cli-mirage'].useDefaultPassthroughs = true;
+
+  return {};
+};
